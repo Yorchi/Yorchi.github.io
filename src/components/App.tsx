52b1ff5a576306b8bdf0dcde6a391785f020e8a8
Yorch <j.andrade.dev@gmail.com>
2025-03-15 00:46:49 -0600

    added under construction

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,4 @@
 import Avatar from 'components/Avatar'
-import logo from 'assets/logo.svg'
 
 const randoms = [
   [1, 2],
@@ -9,56 +8,22 @@ const randoms = [
 
 function App() {
   return (
-    <div className="relative overflow-hidden bg-white">
-      <div className="h-screen sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
-        <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
-          <div className="sm:max-w-lg">
-            <div className="my-4">
-              <Avatar size="large" src={logo} />
-            </div>
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Welcome!
-            </h1>
-            <p className="mt-4 text-xl text-gray-500">
-              This is a boilerplate build with Vite, React 18, TypeScript,
-              Vitest, Testing Library, TailwindCSS 3, Eslint and Prettier.
+    <div className="h-screen flex items-center justify-center bg-base-300">
+      <div className="hero bg-base-200 shadow rounded-xl md:max-w-1/2">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-3xl font-bold">🚀 ¡Estamos trabajando en algo increíble!</h1>
+            <p className="py-6">
+              Mi portafolio estará disponible pronto. Mientras tanto, ¡puedes contactarme aquí!
             </p>
-          </div>
-          <div className="my-10">
-            <a
-              href="vscode://"
-              className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-offset-2"
-            >
-              Start building for free
-            </a>
-            <div
-              aria-hidden="true"
-              className="pointer-events-none mt-10 md:mt-0 lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
-            >
-              <div className="absolute sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
-                <div className="flex items-center space-x-6 lg:space-x-8">
-                  {randoms.map((random, number) => (
-                    <div
-                      key={`random-${random[number]}`}
-                      className="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8"
-                    >
-                      {random.map((number) => (
-                        <div
-                          key={`random-${number}`}
-                          className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100"
-                        >
-                          <img
-                            src={`https://picsum.photos/600?random=${number}`}
-                            alt=""
-                            className="size-full bg-indigo-100 object-cover object-center"
-                          />
-                        </div>
-                      ))}
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
+            <ul>
+              <li>
+                📧 Contáctame en <a href="mailto:j.andrade.dev@gmail.com" className="btn btn-link">j.andrade.dev@gmail.com</a>
+              </li>
+              <li>
+               💼 LinkedIn: <a href="https://www.linkedin.com/in/jorge-andrade-007130b4/" className="btn btn-link" target="_blank">linkedin.com/in/jorge-andrade-007130b4/</a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
